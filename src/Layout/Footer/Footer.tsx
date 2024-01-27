e70d91e55235dcaccbc58d8cefcd9e0032e9efaa
StepanKhachikyan1999 <[stepan-khachikyan@mail.ru]>
2024-01-27 20:46:15 +0400

    added meekzIt

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -68,52 +68,65 @@ function Footer() {
         },
     ];
     return (
-        <div className="bg-dry py-4 bprder=t-2 border-black">
-            <div className="container mx-auto px-2">
-                <div
-                    className="grid grid-cols-2 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-9 lg:gap-11 xl:gap-7 py-10 justify-between">
-                    {Links.map((link, index) => (
-                        <div
-                            key={index}
-                            className="col-span-1 md:col-span-2 lg:col-span-3 pb-3.5 sm:pb-0"
-                        >
-                            <h3 className="text-md lg:leading-7 font-medium mb-4 sm:mb-5 lg:mb-6 pb-0.5">
-                                {link.title}
-                            </h3>
-                            <ul className="text-sm flex flex-col space-y-3">
-                                {link.links.map((text, index) => (
-                                    <li key={index} className="flex items-baseline">
-                                        <Link
-                                            to={text.link}
-                                            className="text-border inline-block w-full hover:text-subMain"
-                                        >
-                                            {text.name}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
+        <div>
+            <div className="bg-dry py-4 bprder=t-2 border-black">
+                <div className="container mx-auto px-2">
+                    <div
+                        className="grid grid-cols-2 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-9 lg:gap-11 xl:gap-7 py-10 justify-between">
+                        {Links.map((link, index) => (
+                            <div
+                                key={index}
+                                className="col-span-1 md:col-span-2 lg:col-span-3 pb-3.5 sm:pb-0"
+                            >
+                                <h3 className="text-md lg:leading-7 font-medium mb-4 sm:mb-5 lg:mb-6 pb-0.5">
+                                    {link.title}
+                                </h3>
+                                <ul className="text-sm flex flex-col space-y-3">
+                                    {link.links.map((text, index) => (
+                                        <li key={index} className="flex items-baseline">
+                                            <Link
+                                                to={text.link}
+                                                className="text-border inline-block w-full hover:text-subMain"
+                                            >
+                                                {text.name}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
 
-                    <div className="pb-3.5 sm:pb-0 col-span-1 md:col-span-2 lg:col-span-3">
-                        <Link to="/">
-                            <img
-                                src="/images/logo.png"
-                                alt="logo"
-                                className="w-2/4 object-contain h-12"
-                            />
-                        </Link>
-                        <p className="leading-7 text-sm text-border mt-3">
+                        <div className="pb-3.5 sm:pb-0 col-span-1 md:col-span-2 lg:col-span-3">
+                            <Link to="/">
+                                <img
+                                    src="/images/logo.png"
+                                    alt="logo"
+                                    className="w-2/4 object-contain h-12"
+                                />
+                            </Link>
+                            <p className="leading-7 text-sm text-border mt-3">
               <span>
                 Armenia <br/> Yerevan
               </span>
-                            <br/>
-                            <span>Tell: +255 754 661 423</span>
-                            <br/>
-                            <span>Email: newFilmss@gmail.com</span>
-                        </p>
+                                <br/>
+                                <span>Tell: +255 754 661 423</span>
+                                <br/>
+                                <span>Email: newFilmss@gmail.com</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div style={{
+                color: 'white',
+                textAlign: 'center',
+                padding: '5px'
+            }}>
+                created by <a style={{
+                color: '#046381',
+                fontSize: '15px'
+            }} target='_blank' href="https://meekz-it.vercel.app/">MeekzIT</a>
             </div>
         </div>
     )
