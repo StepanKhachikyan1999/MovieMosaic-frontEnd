7ffb7cad508b241f440622a6245df445c0b54805
StepanKhachikyan1999 <stepan-khachikyan@mail.ru>
2024-11-23 22:57:34 +0400

    fix: added logo

--- src/Layout/Navbar/Navbar.tsx
+++ src/Layout/Navbar/Navbar.tsx
@@ -11,6 +11,7 @@ import {
 } from '@material-tailwind/react'
 import { FaHeart, FaSearch } from "react-icons/fa";
 import LanguageSelector from "../../hooks/LanguageSelector";
+import logo from "../../logos/film_logo.svg"
 
 function NavBar(): any {
     const [search, setSearch] = useState("")
@@ -131,7 +132,9 @@ function NavBar(): any {
                 <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4 bg-main shadow-md border-none sticky top-0 z-20">
                     <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
                         <Link to='/'>
-                            <Typography as="a" href="#" className="mr-4 cursor-pointer py-1.5 font-medium">LOGO</Typography>
+                            <Typography as="a" href="#" className="mr-4 cursor-pointer py-1.5 font-medium">
+                                <img src={logo} alt="logo"/>
+                            </Typography>
                         </Link>
                         <form onSubmit={handleSearch} className="w-1/2 text-sm bg-dryGray rounded flex gap-4 hidden lg:flex md:max-w-sm">
                             <button type="submit" className="bg-subMain w-12 flex justify-center items-center h-12 rounded text-white">
