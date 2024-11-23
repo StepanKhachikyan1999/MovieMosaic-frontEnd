b509873f02aa0b1d9ede9e89cee605563b0d674c
StepanKhachikyan1999 <stepan-khachikyan@mail.ru>
2024-11-24 01:19:44 +0400

    fix: change manifest name

--- src/Layout/Navbar/Navbar.tsx
+++ src/Layout/Navbar/Navbar.tsx
@@ -178,7 +178,7 @@ function NavBar(): any {
                                 <LanguageSelector />
                             </div>
                         </div>
-                        <IconButton variant="text" className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden mr-3 -mt-4" ripple={false} onClick={() => setOpenNav(!openNav)}>
+                        <IconButton variant="text" className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden mr-3" ripple={false} onClick={() => setOpenNav(!openNav)}>
                             {openNav ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
