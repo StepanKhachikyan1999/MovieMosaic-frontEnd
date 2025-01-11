513efaeef090889c1d52fa4a779b685e7d53e74e
StepanKhachikyan1999 <stepan-khachikyan@mail.ru>
2025-01-11 22:11:54 +0400

    fix configs

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import {BrowserRouter} from 'react-router-dom'
 import './hooks/i18n'
 
 // redux
-import {Provider} from 'react-redux'
-import {store} from './Redux/store'
+import { Provider } from 'react-redux'
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
     resolve: {
-        extensions: [".js", ".jsx"],
+        extensions: [".js", ".jsx", "ts", "tsx"],
         fallback: {
             https: require.resolve('https-browserify'),
         },
@@ -10,5 +10,6 @@ module.exports = {
         contentBase: path.join(__dirname, "public"), // Serve files from this directory
         port: 3000, // Port for the development server
         open: true, // Open the default web browser when the server starts
+        historyApiFallback: true, // Redirects 404s to index.html
     },
 };
